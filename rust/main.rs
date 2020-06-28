695502ad7bada42418548f8aedffbdc7fbc47bad
kinako <ktgi.357exe@gmail.com>
2020-06-28 22:43:43 +0900

    prolog

--- rust/main.rs
+++ rust/main.rs
@@ -6,8 +6,8 @@ fn main(){
     }
 }
 
-fn fizzbuzz(i:i32) -> String {
-    let is_fizz = |i| { i % 3 == 0 };
+fn fizzbuzz(i:i32) -> String { // 引数は32itの整数、返すのはString型
+    let is_fizz = |i| { i % 3 == 0 }; // 無名関数を定義
     let is_buzz = |i| { i % 5 == 0 };
 
     if is_fizz(i) && is_buzz(i) {
