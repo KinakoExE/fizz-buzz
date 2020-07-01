a95730e1989d467e88100f46e8daf118fefdc9c4
kinako <ktgi.357exe@gmail.com>
2020-07-01 22:35:03 +0900

    kotlin

---
+++ kotlin/main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>){
+  for(i in 1..100){
+      var res = fizzbuzz(i)
+      System.out.println(res)
+  }
+}
+
+fun fizzbuzz(i: Int): String{
+  return when {
+    i % 3 == 0 && i % 5 == 0 -> "FizzBuzz"
+    i % 3 == 0 -> "Fizz"
+    i % 5 == 0 -> "Buzz"
+    else -> i.toString()
+  }
+}
